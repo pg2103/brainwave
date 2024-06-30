20a427ce50477c8ef8f9fef784f25b6ae12b6ee3
Prakhar Gupta <prakhar.gupta.212003@gmail.com>
2024-06-30 19:15:04 +0530

    Completed Benefits Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
 import Hero from './components/Hero';
 import Header from './components/Header';
+import  Benefits  from './components/Benefits';
 const App=()=> {
   return (
     <>
     <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
       <Header/>
       <Hero/>
+      <Benefits/>
     </div>
     <ButtonGradient />
     
