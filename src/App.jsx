6a5c8f7602ebee0f1ed7563e56b47cc92ee3885f
Prakhar Gupta <prakhar.gupta.212003@gmail.com>
2024-07-03 11:16:09 +0530

    completed my website by adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Collabration from './components/Collabration';
 import Services from './components/Services';
 import Pricing from './components/Pricing';
 import Roadmap from './components/Roadmap';
+import Footer from './components/Footer';
 const App=()=> {
   return (
     <>
@@ -17,6 +18,7 @@ const App=()=> {
       <Services/>
       <Pricing/>
       <Roadmap/>
+      <Footer/>
     </div>
     <ButtonGradient />
     
